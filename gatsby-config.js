09e49e30c7dd038b004694107d4f38ca91cf1249
raj <raj@rajs-MacBook-Pro.local>
2023-01-24 21:00:47 +0530

    line numbers and other changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,7 @@ module.exports = {
       resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
+          
           'gatsby-remark-relative-images',
           {
             resolve: "gatsby-remark-images",
@@ -77,8 +78,12 @@ module.exports = {
     {
       resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
       options: {
+        printRejected: true, 
         develop: true, // Activates purging in npm run develop
         purgeOnly: ["/all.sass"], // applies purging only on the bulma css file
+        purgeCSSOptions: {
+          safelist: ["blockquote"]
+        }
       },
     }, // must be after other CSS plugins
     "gatsby-plugin-netlify", // make sure to keep it last in the array
