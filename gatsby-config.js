85c33942443655d343c8500a40ca37ebc681802b
raj <raj@rajs-MacBook-Pro.local>
2023-01-24 21:27:56 +0530

    Change og types

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "valmi.io - CDP kernel",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "valmi.io provides an open source CDP platform for the modern data stack.",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
