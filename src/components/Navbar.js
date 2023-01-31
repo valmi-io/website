bccd21518813ca1f7aff8636b9198cfc6e938725
raj <raj@rajs-MacBook-Pro.local>
2023-01-31 13:14:56 +0530

    dark navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,12 @@ const Navbar = () => {
   const [isActive, setIsActive] = useState(false);
 
   return (
-    <nav 
-    // style={{
-    //   backgroundColor:'rgba(255, 255, 255, 0)'
-    // }}
-      className="navbar"
+    <nav
+      // style={{
+      //   backgroundColor:'rgba(255, 255, 255, 0)'
+      // }}
+      className="navbar is-dark
+      "
       role="navigation"
       aria-label="main-navigation"
     >
@@ -31,12 +32,15 @@ const Navbar = () => {
             <span />
           </button>
         </div>
-        <ul id="navMenu" className={` navbar-start navbar-menu ${isActive && "is-active"}`}>
-            {/* TODO: inline override of padding is a result of refactoring
+        <ul
+          id="navMenu"
+          className={` navbar-start navbar-menu ${isActive && "is-active"}`}
+        >
+          {/* TODO: inline override of padding is a result of refactoring
                 to a ul for accessibilty purposes, would like to see a css
                 re-write that makes this unneccesary.
              */}
-            {/* <li className="navbar-item" style={{padding: "0px"}}>
+          {/* <li className="navbar-item" style={{padding: "0px"}}>
               <Link className="navbar-item" to="/about">
                 About
               </Link>
@@ -46,22 +50,22 @@ const Navbar = () => {
               Products
             </Link>
             </li>*/}
-            <li className="navbar-item" style={{padding: "0px"}}>
+          <li className="navbar-item" style={{ padding: "0px" }}>
             <Link className="navbar-item" to="/">
               Home
             </Link>
-            </li>
-            <li className="navbar-item" style={{padding: "0px"}}>
+          </li>
+          <li className="navbar-item" style={{ padding: "0px" }}>
             <Link className="navbar-item" to="/blog">
               Blog
             </Link>
-            </li>
-            <li className="navbar-item" style={{padding: "0px"}}>
+          </li>
+          <li className="navbar-item" style={{ padding: "0px" }}>
             <Link className="navbar-item" to="/contact">
               Contact
             </Link>
-            </li>
-            {/* <li className="navbar-item" style={{padding: "0px"}}>
+          </li>
+          {/* <li className="navbar-item" style={{padding: "0px"}}>
             <Link className="navbar-item" to="/contact/examples">
               Form Examples
             </Link>
