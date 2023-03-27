8af70bbc8447820b940d0e407857f138749816f6
Rajashekar Varkala <raj@mywavia.com>
2023-03-27 21:38:58 +0530

    og-images fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -35,7 +35,7 @@ const TemplateWrapper = ({ children }) => {
 
         <link
           rel="mask-icon"
-          href={`${withPrefix("/")}img/safari-pinned-tab.svg`}
+          href={`${withPrefix("/")}img/valmi_logo_no_text.svg`}
           color="#ff4400"
         />
         <meta name="theme-color" content="#fff" />
